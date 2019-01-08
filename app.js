1b4d7d78a470071cae1acd42120dfc79c6f9f10f
Brendan Quirk <brendanquirk@Brendans-MBP.home>
2019-01-08 14:04:46 -0500

    image and some content displaying on page using ajax and api

--- app.js
+++ app.js
@@ -6,8 +6,14 @@ $(() => {
     (data) => {
       console.log(data);
       console.log(data.results[0].image);
-      const $image = $('<img>').
-      $('.content').append(data.results[0].img)
+      const $image = $('<img>').attr('src', data.results[0].image);
+      $('.content').append($image);
+      const $list = $('<ul>')
+      $('.content').append($list);
+      const $name = $('<li>').text(data.results[0].name);
+      $list.append($name);
+      const $location = $('<li>').text(data.results[0].location.name);
+      $list.append($location);
     },
     () => {
       console.log('Error');
