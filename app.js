f98c73efeea8d94836e0be981cabc5435672d978
Brendan Quirk <brendanquirk@Brendans-MBP.home>
2019-01-08 14:18:47 -0500

    created a for loop to interate through all of the characters on page 1 and display thier name location origin and status

--- app.js
+++ app.js
@@ -5,15 +5,20 @@ $(() => {
   }).then(
     (data) => {
       console.log(data);
-      console.log(data.results[0].image);
-      const $image = $('<img>').attr('src', data.results[0].image);
-      $('.content').append($image);
-      const $list = $('<ul>')
-      $('.content').append($list);
-      const $name = $('<li>').text(data.results[0].name);
-      $list.append($name);
-      const $location = $('<li>').text(data.results[0].location.name);
-      $list.append($location);
+      for (let i = 0; i < data.results.length; i++){
+        const $image = $('<img>').attr('src', data.results[i].image);
+        $('.content').append($image);
+        const $list = $('<ul>')
+        $('.content').append($list);
+        const $name = $('<li>').text('Name: ' + data.results[i].name);
+        $list.append($name);
+        const $location = $('<li>').text('Location: ' + data.results[i].location.name);
+        $list.append($location);
+        const $origin = $('<li>').text('Origin: ' + data.results[i].origin.name);
+        $list.append($origin);
+        const $status = $('<li>').text('Status: ' + data.results[i].status);
+        $list.append($status);
+      }
     },
     () => {
       console.log('Error');
