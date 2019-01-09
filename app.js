9fc7994a814be29dd25d7b2d11f5357af33bd081
Brendan Quirk <brendanquirk@Brendans-MBP.home>
2019-01-08 19:57:31 -0500

    all pages loaded at one time need to add functionality for infinite scroll

--- app.js
+++ app.js
@@ -1,42 +1,29 @@
 $(() => {
 
-const checkScroll = (elem) => {
-  let docTop = $(window).scrollTop();
-  let docBot = docTop + $(window).height();
-
-  let elemTop = $(elem).offset().top;
-  let elemBot = elemTop + $(elem).height();
-
-  return((elemBot <= docBot) && (elemTop >= docTop));
-}
-
-  $.ajax({
-    url: 'https://rickandmortyapi.com/api/character/'
-  }).then(
-    (data) => {
-      console.log(data);
-      for (let i = 0; i < data.results.length; i++){
-        const $image = $('<img>').attr('src', data.results[i].image);
-        $('.content').append($image);
-        const $list = $('<ul>')
-        $('.content').append($list);
-        const $name = $('<li>').text('Name: ' + data.results[i].name);
-        $list.append($name);
-        const $location = $('<li>').text('Location: ' + data.results[i].location.name);
-        $list.append($location);
-        const $origin = $('<li>').text('Origin: ' + data.results[i].origin.name);
-        $list.append($origin);
-        const $status = $('<li>').text('Status: ' + data.results[i].status);
-        $list.append($status);
+  for (let i = 0; i <= 25; i++) {
+    $.ajax({
+      url: 'https://rickandmortyapi.com/api/character/?page=' + i
+    }).then(
+      (data) => {
+        console.log(data);
+        for (let i = 0; i < data.results.length; i++) {
+          const $image = $('<img>').attr('src', data.results[i].image);
+          $('.content').append($image);
+          const $list = $('<ul>')
+          $('.content').append($list);
+          const $name = $('<li>').text('Name: ' + data.results[i].name);
+          $list.append($name);
+          const $location = $('<li>').text('Location: ' + data.results[i].location.name);
+          $list.append($location);
+          const $origin = $('<li>').text('Origin: ' + data.results[i].origin.name);
+          $list.append($origin);
+          const $status = $('<li>').text('Status: ' + data.results[i].status);
+          $list.append($status);
+        }
+      },
+      () => {
+        console.log('Error');
       }
-      $('body').append('<div>').addClass('last')
-    },
-    () => {
-      console.log('Error');
-    }
-  );
-  $('.last').on('scroll', () => {
-    console.log('scroll');
-  })
-
+    );
+  }
 })
